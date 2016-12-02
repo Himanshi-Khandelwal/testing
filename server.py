08bebe398ba6ca09dd92a84c14ac7640571e1c89
himanshi <himanshikhandelwal8@gmail.com>
2016-12-02 23:00:14 +0530

    signup

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request , url_for, redirect
 from content_management import Content
 from flaskext.mysql import MySQL
 Topic_dict=Content()
+
+
 import MySQLdb
 
 mysql = MySQL()
@@ -16,9 +18,14 @@ mysql.init_app(app)
 def home():
  return render_template("main.html")
 
-@app.route('/dashboard/<int:username>')
+@app.route('/dashboard/<username>')
 def dashboard(username):
  return render_template("dashboard.html",Topic_dict=Topic_dict)
+
+@app.route('/welcome/<username>')
+def welcome(username):
+ return render_template("welcome.html")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -32,7 +39,7 @@ def connection():
 #    row=c.fetchone()
      #print row[0]
 #    c.close()
-#    db.close()	
+#    db.close()
 
 #@app.route('/login/',methods=['GET','POST'])
 #def login():
@@ -48,7 +55,7 @@ def connection():
 #     c.commit()
 #     row=c.fetchone()
 #     c.close()
-#     db.close()	
+#     db.close()
 #     return redirect(url_for('dashboard'))
 #     #if attempted_username=="admin" and attempted_password=="password":
 #    else:
@@ -64,11 +71,11 @@ def connection():
 
 @app.route('/login/',methods=['GET','POST'])
 def login():
-    if request.method == 'POST':	
+    if request.method == 'POST':
 	    username = request.form['username']
 	    password = request.form['password']
 	    cursor = mysql.connect().cursor()
-	    cursor.execute("SELECT * from login_data where Username='" + username + "' and Password='" + password + "'")
+	    cursor.execute("SELECT * from signup where Username='" + username + "' and Password='" + password + "'")
 	    data = cursor.fetchone()
 	    if data is None:
              print "Error"
@@ -76,6 +83,45 @@ def login():
 	     return redirect(url_for('dashboard',username = username))
     return render_template("login.html")
 
+
+#@app.route('/signup/',methods=['GET','POST'])
+#def signup():
+#    if request.method == 'POST':
+#	    username = request.form['username']
+#	    email = request.form['email']
+#	    password = request.form['password']
+#	    mobile = request.form['mobile']
+
+
+#	    cursor = mysql.connect().cursor()
+#	    print "INSERT INTO signup values('"+username+"','"+email+"','"+password+"','"+mobile+"');"
+#	    cursor.execute("INSERT INTO signup values('"+username+"','"+email+"','"+password+"','"+mobile+"');")
+#	    #data = cursor.fetchone()
+#	    #if data is None:
+#             #print "Error"
+#	    #else:
+#	    return redirect(url_for('welcome',username = username))
+#    return render_template("signup.html")
+
+
+@app.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    error = None
+    if request.method == 'POST':
+       # cursor = mysql.connect().cursor()
+	conn=mysql.connect()
+	cursor=conn.cursor()
+        cursor.execute("SELECT * from signup where username='" + request.form['username'] + "' and email='" + request.form['email'] + "' and password='" + request.form['password'] + "' and mobile='" + request.form['mobile'] + "'")
+#	conn=mysql.connect()
+#	cursor=conn.cursor()
+        data = cursor.fetchone()
+        if data is None:
+            cursor.execute("INSERT INTO signup VALUES ('" + request.form['username'] + "', '" + request.form['email'] + "', '" + request.form['paaword'] + "','" + request.form['mobile'] + "')")
+            conn.commit
+            redirect(url_for('welcome'))
+        else:
+            error = "it is complete"
+    return render_template('/signup.html/', error=error)
 
 
 
